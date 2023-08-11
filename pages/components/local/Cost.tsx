e78d746d8de68af581e3fc9d7af72bee8d5e5ff4
Asadulloh <sayfullayevasadulla1@gmail.com>
2023-08-11 22:49:04 +0500

    fixed some bugs at blog and other blocks)

--- pages/components/local/Cost.tsx
+++ pages/components/local/Cost.tsx
@@ -114,43 +114,21 @@ const Cost = () => {
                                             } : {}}>
                                                 {e}
                                             </div>
-
                                         </div>
                                     })}
                                 </div>
                                 <div className={styles.selection}>
                                     <div className={styles.container}>
-                                        <h3>Тип помещения</h3>
-                                        <div className={styles.checks}>
-                                            {ads.map((e: string) => {
-                                                return <div key={e} className={styles.checkbox}>
-                                                    <input onClick={() => {
-                                                        setChecked(!checked)
-                                                        setSelected(e)
-                                                    }} checked={checked} style={e === selected ? {
-                                                        background: "#46247c"
-                                                    } : {}} type="checkbox" name="" id="" />
-                                                    <p>{e}</p>
-                                                </div>
-                                            })}
-                                        </div>
-                                        <div className={styles.images}>
-                                            <Image src={"/images/select1.png"} alt='decorate' width={380} height={245} />
-                                            <Image src={"/images/select2.png"} alt='decorate' width={380} height={245} />
-                                        </div>
-                                        <h3>Доп.услуги</h3>
-                                        <div className={styles.checks}>
-                                            {ads2.map((e: string) => {
-                                                return <div key={e} className={styles.checkbox}>
-                                                    <input onClick={() => {
-                                                        setChecked(!checked)
-                                                        setSelected2(e)
-                                                    }} checked={checked} style={e === selected2 ? {
-                                                        background: "#46247c"
-                                                    } : {}} type="checkbox" name="" id="" />
-                                                    <p>{e}</p>
-                                                </div>
-                                            })}
+                                        <h3>Площадь и количество комнат</h3>
+                                        <div className={styles.sectS}>
+                                            <div className={styles.sect}>
+                                                <h4>Выберите общую площадь квартиры</h4>
+                                                <div className={styles.multiRange}>169 м²</div>
+                                            </div>
+                                            <div className={styles.sect}>
+                                                <h4>Количество комнат</h4>
+                                                <div></div>
+                                            </div>
                                         </div>
                                         <button onClick={() => {
                                             setCounter(counter + 1)
@@ -179,7 +157,7 @@ const Cost = () => {
                                     </div>
                                     <div className={styles.selection}>
                                         <div className={styles.container}>
-                                            <h3>Тип помещения</h3>
+                                            <h3>Стиль ремонта</h3>
                                             <div className={styles.checks}>
                                                 {ads.map((e: string) => {
                                                     return <div key={e} className={styles.checkbox}>
@@ -238,7 +216,7 @@ const Cost = () => {
                                         </div>
                                         <div className={styles.selection}>
                                             <div className={styles.container}>
-                                                <h3>Тип помещения</h3>
+                                                <h3>Итого</h3>
                                                 <div className={styles.checks}>
                                                     {ads.map((e: string) => {
                                                         return <div key={e} className={styles.checkbox}>
