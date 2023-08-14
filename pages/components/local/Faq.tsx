e6f6b07f391edc752d6cf2e8730cc6a2617d4732
Abdumalikov Asliddin <asliddinabdumalikov8002@gmail.com>
2023-08-14 23:19:19 +0500

    card transion fixed

--- pages/components/local/Faq.tsx
+++ pages/components/local/Faq.tsx
@@ -13,47 +13,24 @@ const Faq = () => {
           <div className={styles.faqTitle}>
             <h1>ЧАСТО ЗАДАВАЕМЫЕ ВОПРОСЫ</h1>
           </div>
-          <div className={styles.faqs}>
+          <div className={styles.quests}>
             {[1, 2, 3, 4, 5].map((e: number) => {
-              const id = uuidv4();
-              return (
-                <div
-                  key={id}
-                  onClick={() => {
-                    setIsOpened(!isOpened);
-                    setIsSelected(e);
-                  }}
-                  className={styles.faqCard}
-                >
-                  <div className={styles.faqTop}>
-                    <h3>Текст вопроса?</h3>
-                    <button
-                      onClick={() => {
-                        setIsOpened(!isOpened);
-                      }}
-                    >
-                      <Image
-                        style={
-                          isSelected !== e
-                            ? {
-                                transform: "rotate(-180deg)",
-                              }
-                            : {}
-                        }
-                        src={"icons/chevron.svg"}
-                        alt="chevron icon"
-                        width={10}
-                        height={18}
-                      />
-                    </button>
-                  </div>
-                  <p className={isSelected !== e ? styles.dn : styles.just}>
-                    Наша команда профессиональных дизайнеров разработают дизайн
-                    проект Вашего помещения по Вашему желанию. При заказе услуги
-                    Платинум, дизайн в подарок
-                  </p>
+              return <div key={e} onClick={() => {
+                setIsOpened(!isOpened)                     
+                setIsSelected(e)
+              }} className={styles.quest}>
+                <div className={styles.questTop}>
+                  <h3>Текст вопроса?</h3>
+                  <button onClick={() => {     
+                    setIsOpened(!isOpened)
+                  }}>
+                    <Image style={isSelected !== e ? {
+                      transform: "rotate(-180deg)"
+                    } : {}} src={"/icons/chevron.svg"} alt='chevron icon' width={10} height={17} />
+                  </button>
                 </div>
-              );
+                <p className={isSelected !== e ? styles.dn : styles.just}>Наша команда профессиональных дизайнеров разработают дизайн проект Вашего помещения по Вашему желанию. При заказе услуги Платинум, дизайн в подарок</p>
+              </div>
             })}
           </div>
         </div>
